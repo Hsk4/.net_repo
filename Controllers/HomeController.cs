8843d07ea86e057e692b94963ba502ee35b2b141
Hsk4 <155343161+Hsk4@users.noreply.github.com>
2024-09-28 01:46:32 +0500

    made a file

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using app2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace app2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult page1()
+        {
+            return View();
+        }
+        public IActionResult page2()
+        {
+            return View();
+        }
+        public IActionResult page3()
+        {
+            return View();
+        }
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
